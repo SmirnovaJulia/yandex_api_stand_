e1f19510e7c166e2abcac2f6664c50b6fbbed971
SmirnovaJulia <sjulias199@mail.ru>
2023-09-16 00:20:27 +0400

    My commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://fb33178f-28d3-4a57-a929-bf8a10b333d0.serverhub.praktikum-services.ru"  # адрес сервера
+CREATE_ORDER = "/api/v1/orders"  # создание заказа
+ORDER_INFORMATION = "/api/v1/orders/track?t="  # получить информацию о заказе по его номеру
